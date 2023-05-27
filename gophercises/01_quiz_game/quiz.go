2fe71511e2f330010cb2790beb972cb8c92cade2
Jonathan Lamb <jlamb.8153@gmail.com>
2023-05-27 02:31:55 +0200

    complete exercise 1

--- gophercises/01_quiz_game/quiz.go
+++ gophercises/01_quiz_game/quiz.go
@@ -32,12 +32,6 @@ func parseLines(lines [][]string) []problem {
     return ret
 }
 
-// TODO: finish implementing me, see https://gobyexample.com/goroutines
-func timer(seconds int32, score int, numQuestions int) {
-    time.Sleep(time.Duration(seconds) * time.Second)
-    exit(fmt.Sprintf("The timer ran out! You scored %d out of %d questions correctly.\n", score, numQuestions))
-}
-
 /*
    Part 2 requirements:
     - add a timer that can be updated via a flag, default to 30 seconds
@@ -47,6 +41,7 @@ func timer(seconds int32, score int, numQuestions int) {
 func main() {
     // The `:=` syntax is shorthand for declaring and initialising a variable
     csvPath := flag.String("csv", "problems.csv", "A CSV file in the format of 'question,answer' (default \"problems.csv\")")
+    timeLimit := flag.Int("limit", 30, "The time limit for the quiz in seconds (default 30 seconds)")
     flag.Parse()
 
     // `*` dereferences the pointer and gets the value stored at that address in memory
@@ -69,32 +64,47 @@ func main() {
 
     problems := parseLines(records)
 
-    for i, p := range problems {
-        answer, err := strconv.ParseInt(p.a, 0, 32) // https://gobyexample.com/number-parsing
-        if err != nil {
-            exit(fmt.Sprintf("Error parsing answer '%s' from record\n", p.a))
+    timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
+    // You can use this to explicitly break specific sections
+    problemloop:
+        for i, p := range problems {
+            fmt.Printf("Problem #%d: %s = ", i + 1, p.q)
+
+            answer, err := strconv.ParseInt(p.a, 0, 32) // https://gobyexample.com/number-parsing
+            if err != nil {
+                exit(fmt.Sprintf("Error parsing answer '%s' from record\n", p.a))
+            }
+
+            answerCh := make(chan int64)
+
+            go func() {
+                var userInput string
+                _, err = fmt.Scanln(&userInput) // `&` symbol is used to get the memory address
+                if err != nil {
+                    exit(fmt.Sprintf("Error reading input '%s'\n", userInput))
+                }
+
+                i, err := strconv.ParseInt(userInput, 0, 32)
+                if err != nil {
+                    exit("Error parsing integer from user input")
+                }
+
+                answerCh <- i
+            }()
+
+            select {
+            case <-timer.C:
+                break problemloop
+            case i :=<-answerCh:
+                if i == answer {
+                    fmt.Println("Correct!")
+                    score++
+                } else {
+                    fmt.Println("Incorrect :(")
+                }
+            }
         }
 
-        fmt.Printf("Problem #%d: %s = ", i + 1, p.q)
-
-        var userInput string
-        _, err = fmt.Scanln(&userInput) // `&` symbol is used to get the memory address
-        if err != nil {
-            exit(fmt.Sprintf("Error reading input '%s'\n", userInput))
-        }
-
-        i, err := strconv.ParseInt(userInput, 0, 32)
-        if err != nil {
-            exit("Error parsing integer from user input")
-        }
-
-        if i == answer {
-            fmt.Println("Correct!")
-            score++
-        } else {
-            fmt.Println("Incorrect :(")
-        }
-    }
-
-    fmt.Printf("You scored %d out of %d questions correctly.\n", score, len(problems))
+    fmt.Printf("\nYou scored %d out of %d questions correctly.\n", score, len(problems))
 }
