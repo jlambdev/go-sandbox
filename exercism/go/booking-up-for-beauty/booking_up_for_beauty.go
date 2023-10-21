e2ce17656c899e3e040ffc6d20ca196e4fdcb105
Jonathan Lamb <jlamb.8153@gmail.com>
2023-10-21 19:35:47 +0200

    go time

---
+++ exercism/go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -0,0 +1,47 @@
+package booking
+
+import (
+	"time"
+)
+
+// Schedule returns a time.Time from a string containing a date.
+func Schedule(date string) time.Time {
+	layout := "1/2/2006 15:04:05"
+	t, err := time.Parse(layout, date)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+// HasPassed returns whether a date has passed.
+func HasPassed(date string) bool {
+	layout := "January 2, 2006 15:04:05"
+	t, err := time.Parse(layout, date)
+	if err != nil {
+		panic(err)
+	}
+	return t.Before(time.Now())
+}
+
+// IsAfternoonAppointment returns whether a time is in the afternoon.
+func IsAfternoonAppointment(date string) bool {
+	layout := "Monday, January 2, 2006 15:04:05"
+	t, err := time.Parse(layout, date)
+	if err != nil {
+		panic(err)
+	}
+	return t.Hour() >= 12 && t.Hour() < 18
+}
+
+// Description returns a formatted string of the appointment time.
+func Description(date string) string {
+	t := Schedule(date)
+	return t.Format("You have an appointment on Monday, January 2, 2006, at 15:04.")
+}
+
+// AnniversaryDate returns a Time with this year's anniversary.
+func AnniversaryDate() time.Time {
+	t := Schedule("9/15/2012 00:00:00")
+	return time.Date(time.Now().Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), time.UTC)
+}
